736c3a5854d8506b9d7d981e963b28800ef9014e
1101137126 <1101137126@gm.kuas.edu.tw>
2015-04-17 16:42:01 +0800

    homework

---
+++ KuasCore/Interceptors/UpdateEmployeeNameInterceptor .cs
@@ -0,0 +1,31 @@
+﻿using AopAlliance.Intercept;
+using KuasCore.Models;
+using System;
+using System.Diagnostics;
+
+namespace KuasCore.Interceptors
+{
+    class UpdateEmployeeNameInterceptor : IMethodInterceptor
+    {
+
+        public object Invoke(IMethodInvocation invocation)
+        {
+            Console.WriteLine("DebogLogInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+            Debug.Print("DebogLogInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+
+            object result = invocation.Proceed();
+            if (result is Employee)
+            {
+                Employee epl = (Employee) result;
+                epl.Name = epl.Name + "haha";
+                result = epl;
+
+            }
+
+            Console.WriteLine("回傳的資料已取得 [{0}]", result);
+            Debug.Print("回傳的資料已取得 [{0}]", result);
+
+            return result;
+        }
+    }
+}
